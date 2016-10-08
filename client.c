74e0604fb33deb09dd52f9abda878475d8397e2e
BrunoBob <tessierb95@gmail.com>
2016-10-08 00:41:25 -0700

    added error management

--- client.c
+++ client.c
@@ -14,23 +14,37 @@ int main(int argc, char* argv[]){
 
   client_socket = socket(AF_INET,SOCK_STREAM, 0); //Create the socket descriptor
 
+  if(client_socket == -1){
+    perror("ERROR : can't create socket");
+    exit(1);
+  }
+
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = inet_addr(argv[1]);  //Give IP adress
   addr.sin_port = htons(atoi(argv[2])); //Give port
 
-connect(client_socket, (struct sockaddr *) &addr, sizeof(struct sockaddr)); //connect to the server
+  if(connect(client_socket, (struct sockaddr *) &addr, sizeof(struct sockaddr)) == -1){//connect to the server
+    perror("ERROR : can't connect to the server");
+    exit(1);
+  }
 
   do{
     scanf("%c", buffer); //Ask a character to user
 
-    send(client_socket, buffer, sizeof(char), 0); //Send the character
+    if(send(client_socket, buffer, sizeof(char), 0) != 1){ //Send the character
+      perror("ERROR : can't send the character");
+      exit(1);
+    }
 
     *buffer = 0 ;  //Reset the buffer
 
-    recv(client_socket, buffer, sizeof(char), 0); //Read the server message
+    if(recv(client_socket, buffer, sizeof(char), 0) != 1){ //Read the server message
+      perror("ERROR : can't recv the character");
+      exit(1);
+    }
 
-    printf("Echo of %c\n", *buffer); //Print the recv message
-    
+    printf("Echo of %c\n", *buffer); //Print the received message
+
   }while(*buffer != 'q'); //Stop when end character 'q'
 
   return 0;
